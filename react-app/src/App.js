25ed9c2a39a421d21f18933ed334b7f92b02fdda
aaman1609 <aaman1609@gmail.com>
2023-03-29 01:18:34 +0530

    SMSIPS Specific

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-
+import pdfFile from "./pdfFile.pdf";
 import "./App.css";
 
 function App() {
@@ -7,21 +7,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SMS<br></br> IPS
-        </p>
+        <p>SMSIPS</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/people/Shri-maa-sharda-international-public-school/100067381647049/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Facebook Link
         </a>
         <br></br>
         <br></br>
 
-        <a href="./pdfFile.pdf">hello</a>
+        <a href={pdfFile}>CBSE Mandatory Disclosure</a>
       </header>
     </div>
   );
