d197d1f3cfdb2aaeb9f7b891b620ef8aa553e4d8
aaman1609 <aaman1609@gmail.com>
2023-03-29 02:52:40 +0530

    SMSIPS_Updates_02

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.jpg";
+/* import logo from "./logo.jpg";
 import pdfFile from "./pdfFile.pdf";
 import "./App.css";
 
@@ -21,6 +21,34 @@ function App() {
 
         <a href={pdfFile}>CBSE Mandatory Disclosure</a>
       </header>
+    </div>
+  );
+}
+
+export default App; */
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="%PUBLIC_URL%/logo192.png" alt="School Logo" />
+        <h1>Shri Maa Sharda International Public School</h1>
+      </header>
+      <main>
+        <p>
+          Welcome to our CBSE school website! Here you can find information
+          about our curriculum, faculty, extracurricular activities, and more.
+          We are committed to providing a quality education and nurturing the
+          whole student. Explore our site to learn more.
+        </p>
+      </main>
+      <footer>
+        <p>
+          © 2023 Shri Maa Sharda International Public School. All rights
+          reserved.
+        </p>
+      </footer>
     </div>
   );
 }
