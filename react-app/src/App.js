cf92e6adae797c17c80cc9ad5eebe345976bbbed
aaman1609 <aaman1609@gmail.com>
2023-03-31 04:24:14 +0530

    SMSIPS_Updates_05

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,139 +1,198 @@
-/* import pdfFile from "./pdfFile.pdf";
+import React from "react";
+import logo from "./SMSIPS_logo.png";
+import aboutImage1 from "./Image_01.JPG";
+import aboutImage11 from "./Image_11.JPG";
+import aboutImage3 from "./Image_03.png";
+import aboutImage4 from "./Image_04.JPG";
+import aboutImage5 from "./Image_05.JPG";
+import aboutImage6 from "./Image_06.JPG";
+import aboutImage7 from "./Image_07.JPG";
+import aboutImage8 from "./Image_08.JPG";
+import aboutImage9 from "./Image_09.JPG";
+import aboutImage10 from "./Image_10.JPG";
+
+import pdf1 from "./Mandatory_Public_Disclosure.pdf";
+import pdf2 from "./AFFILATION_LETTER.pdf";
+import pdf3 from "./Trust Deed.pdf";
+import pdf4 from "./NOC state board.pdf";
+import pdf5 from "./Building safety certificate.pdf";
+import pdf6 from "./Fire Safety 001.pdf";
+import pdf7 from "./hearth and Sanitation  certificate.pdf";
+import pdf8 from "./Fee Structure.pdf";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Shri Maa Sharda International Public School.</p>
-        <a
-          className="App-link"
-          href="https://www.facebook.com/people/Shri-maa-sharda-international-public-school/100067381647049/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Facebook Link
-        </a>
-        <br></br>
-        <br></br>
-
-        <a href={pdfFile}>CBSE Mandatory Disclosure</a>
-      </header>
-    </div>
-  );
-}
-
-export default App; */
-
-import React from "react";
-import logo from "./logo.jpg";
-import "./App.css";
-import pdfFile from "./pdfFile.pdf";
-
-function App() {
-  return (
-    <div>
       <header>
-        <img src={logo} alt="School Logo" />
-        <h1>Shri Maa Sharda International Public School</h1>
+        <div className="logo">
+          <img src={logo} alt="School Logo" />
+          <div className="school-info">
+            <h1>Shri Maa Sharda International Public School</h1>
+            <p className="affiliation">
+              Affiliated to CBSE New Delhi, Affiliation Number: 2133305
+            </p>
+          </div>
+        </div>
         <nav>
           <ul>
             <li>
               <a href="#about">About</a>
             </li>
             <li>
-              <a href="#CBSEMandatoryDisclosure">Disclosure</a>
-            </li>
-            <li>
-              <a href="#academics">Academics</a>
+              <a href="#cbse">CBSE Mandatory Disclosure</a>
             </li>
             <li>
               <a href="#admissions">Admissions</a>
             </li>
             <li>
-              <a href="#events">Events</a>
-            </li>
-            <li>
-              <a href="#careers">Careers</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
+              <a href="#contact">Contact Us</a>
             </li>
           </ul>
         </nav>
       </header>
       <main>
+        <div className="about-image">
+          <img src={aboutImage1} alt="About School" />
+          <img src={aboutImage11} alt="About School" />
+          <img src={aboutImage6} alt="About School" />
+          <img src={aboutImage4} alt="About School" />
+          <img src={aboutImage5} alt="About School" />
+        </div>
+        <div className="about-image">
+          <img src={aboutImage3} alt="About School" />
+          <img src={aboutImage7} alt="About School" />
+          <img src={aboutImage8} alt="About School" />
+          <img src={aboutImage9} alt="About School" />
+          <img src={aboutImage10} alt="About School" />
+        </div>
         <section id="about">
-          <h2>About Us</h2>
-          <p>
-            Shri Maa Sharda International Public School is a premier CBSE school
-            located in the heart of India. We are committed to providing a
-            quality education that emphasizes academic excellence, character
-            development, and community engagement.
-          </p>
-          <p>
-            Our school has state-of-the-art facilities, a dedicated faculty, and
-            a wide range of extracurricular activities to help students discover
-            their passions and develop their talents.
-          </p>
+          <div className="about-content">
+            <h2>About Us</h2>
+            <p>
+              Shri Maa Sharda International Public School is an educational
+              institution committed to providing high-quality education to its
+              students. Our mission is to create a nurturing and challenging
+              learning environment that promotes academic excellence,
+              creativity, and character development.
+            </p>
+            <p>
+              Our dedicated and experienced faculty is committed to helping each
+              student reach their full potential. We offer a well-rounded
+              education that emphasizes not only academic achievement but also
+              personal growth, social responsibility, and community involvement.
+            </p>
+            <p>
+              Our school provides a variety of opportunities for students to
+              explore their interests and develop their talents, including
+              extracurricular activities, clubs, and sports teams. We also offer
+              a range of resources to support students in their academic
+              pursuits, such as a well-stocked library, computer lab, and
+              science lab.
+            </p>
+            <p>
+              At Shri Maa Sharda International Public School, we are proud of
+              our commitment to providing a quality education to all students.
+              We strive to foster a love of learning and prepare our students
+              for success in the 21st century."
+            </p>
+          </div>
         </section>
-        <section id="CBSEMandatoryDisclosure">
+        <section id="cbse">
           <h2>CBSE Mandatory Disclosure</h2>
-          <p>
-            <a href={pdfFile}>CBSE Mandatory Disclosure PDF</a>
-          </p>
-        </section>
-        <section id="academics">
-          <h2>Academics</h2>
-          <p>
-            Our school offers a rigorous academic program that prepares students
-            for success in college and beyond. We offer a wide range of courses
-            in the arts, sciences, and humanities, and our faculty are committed
-            to helping students develop critical thinking skills, creativity,
-            and a lifelong love of learning.
-          </p>
-        </section>
-        <section id="admissions">
-          <h2>Admissions</h2>
-          <p>
-            We welcome students of all backgrounds and abilities to apply to our
-            school. Our admissions process is designed to identify students who
-            will thrive in our rigorous academic program and who share our
-            commitment to community service and character development.
-          </p>
-          <p>
-            To learn more about our admissions process, please visit our
-            admissions page or contact our admissions office.
-          </p>
-        </section>
-        <section id="events">
-          <h2>Events</h2>
-          <p>
-            We host a variety of events throughout the school year to promote
-            community engagement and showcase our students' talents. From music
-            and theater performances to athletic competitions and academic
-            competitions, there is always something exciting happening at Shri
-            Maa Sharda International Public School.
-          </p>
-        </section>
-        <section id="Careers">
-          <h2>Careers</h2>
-          <p>Careers</p>
+          <table>
+            <tbody>
+              <tr>
+                <td>Some content</td>
+                <td>
+                  <a href={pdf1} target="_blank" rel="noreferrer">
+                    Mandatory Public Disclosure
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Some content</td>
+                <td>
+                  <a href={pdf2} target="_blank" rel="noreferrer">
+                    AFFILATION LETTER
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Some content</td>
+                <td>
+                  <a href={pdf3} target="_blank" rel="noreferrer">
+                    Trust Deed
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Some content</td>
+                <td>
+                  <a href={pdf4} target="_blank" rel="noreferrer">
+                    NOC state board
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Some content</td>
+                <td>
+                  <a href={pdf5} target="_blank" rel="noreferrer">
+                    Building Safety certificate
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Some content</td>
+                <td>
+                  <a href={pdf6} target="_blank" rel="noreferrer">
+                    Fire Safety certificate
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Some content</td>
+                <td>
+                  <a href={pdf7} target="_blank" rel="noreferrer">
+                    Health and Sanitation certificate
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Some content</td>
+                <td>
+                  <a href={pdf8} target="_blank" rel="noreferrer">
+                    Fee Structure
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </section>
         <section id="contact">
           <h2>Contact Us</h2>
-          <p>
-            If you have any questions or would like to learn more about our
-            school, please don't hesitate to contact us. You can reach us by
-            phone, email, or by filling out the contact form on our website.
-          </p>
+          <div className="contact-info">
+            <p>
+              <strong>Address:</strong> SHRI MAA SHARDA INTERNATIONAL PUBLIC
+              SCHOOL <br />
+              NEAR AIT BILAYAN TOLL PLAAZA, NH 27 JHANSI KANPUR HIGHWAY AIT{" "}
+              <br />
+              UTTAR PRADESH, JALAUN, 285201
+            </p>
+            <p>
+              <strong>Email ID:</strong> smsipsb@gmail.com
+            </p>
+            <p>
+              <strong>Contact Number:</strong> 8962560564 / 8765550925
+            </p>
+          </div>
         </section>
       </main>
       <footer>
         <p>
-          © 2023 Shri Maa Sharda International Public School. All rights
-          reserved.
+          © 2023 Shri Maa Sharda International Public School. All Rights
+          Reserved.
         </p>
       </footer>
     </div>
